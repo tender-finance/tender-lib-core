a7c472b80f35c90e12fafb6570e19fab9fe1da77
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-06-02 19:18:38 -0400

    add setOracle to oracle interface

--- src/external/oracle/ITenderPriceOracle.sol
+++ src/external/oracle/ITenderPriceOracle.sol
@@ -1,19 +1,15 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.10;
+import {ICToken} from '../compound/ICToken.sol';
+import {IERC20Metadata as IERC20} from "oz/interfaces/IERC20Metadata.sol";
+import {IChainlinkPriceOracle} from './IChainlinkPriceOracle.sol';
 
-interface TenderPriceOracle {
-  function decimals(address token) external view returns (uint8);
-  function latestRoundData(address token) external view returns (
-    uint80 roundId,
-    int256 answer,
-    uint256 startedAt,
-    uint256 updatedAt,
-    uint80 answeredInRound
-  );
+interface ITenderPriceOracle {
+  function getOracleDecimals(IERC20 token) external view returns (uint);
+  function getUSDPrice(IERC20 token) external view returns (uint);
 
-  function getOracleDecimals(address token) external view returns (uint);
-  function getUSDPrice(address token) external view returns (uint);
+  function getUnderlyingDecimals(ICToken ctoken) external view returns (uint);
+  function getUnderlyingPrice(ICToken ctoken) external view returns (uint);
 
-  function getUnderlyingDecimals(address ctoken) external view returns (uint);
-  function getUnderlyingPrice(address ctoken) external view returns (uint);
+  function setOracle(IERC20 token, IChainlinkPriceOracle oracle) external;
 }
