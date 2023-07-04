22f8dbddc233f30e588db97e4ac7978b4a77cdda
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-07-04 15:02:16 -0400

    rename to IComptroller

---
+++ script/Deploy.s.sol
@@ -0,0 +1,8 @@
+// import {Script} from 'forge-std/Script.sol';
+// import {DeployOracle} from './deploy/DeployOracle.sol';
+//
+// contract Deploy is Script {
+//   function run() public {
+//     DeployOracle.deploy();
+//   }
+// }
