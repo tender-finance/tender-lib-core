59709983f37103f1f308600fc34c5bf3f3fca49a
papapalli <papapalli@localhost>
2024-03-20 01:11:28 -0700

    revive Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,8 +1,11 @@
-// import {Script} from 'forge-std/Script.sol';
-// import {DeployOracle} from './deploy/DeployOracle.sol';
-//
-// contract Deploy is Script {
-//   function run() public {
-//     DeployOracle.deploy();
-//   }
-// }
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import {Script} from 'forge-std/Script.sol';
+import {DeployOracle} from './deploy/DeployOracle.sol';
+
+contract Deploy is Script {
+  function run() public {
+    DeployOracle.deploy();
+  }
+}
